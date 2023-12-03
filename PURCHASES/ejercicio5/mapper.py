50af5b553d70c3c83446fdf7ea69fe15f752c7c9
ivaan1903 <ivangarciagonzale2002@gmail.com>
2023-12-03 17:19:39 +0100

    ejercicio5

---
+++ PURCHASES/ejercicio5/mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+    try:
+        data = line.strip().split("\t")
+        date, time, store, item, cost, payment = data
+        print("Total ventas\t"+cost)
+    except ValueError:
+        continue
+
